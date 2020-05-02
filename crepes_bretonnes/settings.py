1dd5eea04f4ea7343ef58db75d0fcc5013bb76bb
standemdem <37410638+standemdem@users.noreply.github.com>
2020-05-02 15:05:14 +0200

    moved private info to separate file added to gitigore

--- crepes_bretonnes/settings.py
+++ crepes_bretonnes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from private import KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'csd54=+%i37u7^113&o&&kiwj3^=9tsj7znwrc_6^_rgtqb6s8'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
